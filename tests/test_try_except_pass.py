1a4cdade006e812705e0665136ec126b73b4348a
Pavel Dedík <pavel.dedik@twisto.cz>
2022-05-24 15:14:30 +0200

    Count return stmt as silent handling in jg04 rule

--- tests/test_try_except_pass.py
+++ tests/test_try_except_pass.py
@@ -4,7 +4,7 @@ from .utils import error_code_in_result, load_fixture_file, run_check
 def test_is_try_except_pass():
     code = load_fixture_file("try_except_pass.py")
     result = run_check(code)
-    assert len(result) == 2
+    assert len(result) == 4
     assert error_code_in_result("JG04", result)
 
 

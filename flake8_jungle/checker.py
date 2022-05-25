e75c0a9abec028cd1de1b3761513c9003349eb64
Pavel Dedík <pavel.dedik@twisto.cz>
2022-05-25 10:34:09 +0200

    Bump version to 1.0.3

--- flake8_jungle/checker.py
+++ flake8_jungle/checker.py
@@ -94,7 +94,7 @@ class JungleStyleChecker:
     """
 
     name = "flake8-twisto"
-    version = "1.0.2"
+    version = "1.0.3"
 
     _options = None
     _option_fields = dataclasses.fields(RuleOptions)

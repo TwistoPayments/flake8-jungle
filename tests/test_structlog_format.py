617522db9403a6ac03392d9ec7b5ba15fa64d1c8
Pavel Dedik <dedikx@gmail.com>
2022-06-23 15:44:21 +0200

    Improve structlog checking, allow f-strings and so on

--- tests/test_structlog_format.py
+++ tests/test_structlog_format.py
@@ -4,7 +4,7 @@ from .utils import error_code_in_result, load_fixture_file, run_check
 def test_is_not_structlog_format():
     code = load_fixture_file("bad_structlog_format.py")
     result = run_check(code, rule_id="JG11")
-    assert len(result) == 4
+    assert len(result) == 6
     assert error_code_in_result("JG11", result)
 
 

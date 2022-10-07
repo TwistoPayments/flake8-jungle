b2079ecc229743b234758cbe9f497c0201374b30
Tomáš Morkus <tmmorkus@gmail.com>
2022-10-07 12:38:29 +0200

    Skip doctring length in FunctionTooLongRule

--- tests/fixtures/function_too_long.py
+++ tests/fixtures/function_too_long.py
@@ -1,4 +1,14 @@
 def foo(a=5):
+    """Function for testing purpose
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dignissim egestas ligula in porta.
+    Nulla molestie neque vel tortor pellentesque lacinia. Phasellus eget malesuada lorem. Aliquam sollicitudin nisl
+    urna, sit amet ullamcorper ex tincidunt at. Donec pretium arcu sed turpis gravida pellentesque. Aenean fringilla
+    nisl et tempus imperdiet. Curabitur ut velit eu arcu gravida ultricies. Maecenas eget consequat quam.
+
+    :param a: random int input
+    :return: random useful string
+    """
     for _ in range(10):
         a += 1
     print("bar")
--- tests/test_function_too_long.py
+++ tests/test_function_too_long.py
@@ -1,13 +1,20 @@
+import pytest as pytest
+
 from flake8_jungle.rules import RuleOptions
 
 from .utils import error_code_in_result, load_fixture_file, run_check
 
 
-def test_is_function_too_long():
+@pytest.mark.parametrize(
+    "function_max_length, function_result",
+    [(10, 1), (30, 1), (31, 0), (500, 0)],
+)
+def test_is_function_too_long(function_max_length: int, function_result: int):
     code = load_fixture_file("function_too_long.py")
-    result = run_check(code, options=RuleOptions(max_function_length=10))
-    assert len(result) == 1
-    assert error_code_in_result("JG06", result)
+    result = run_check(code, options=RuleOptions(max_function_length=function_max_length))
+    assert len(result) == function_result
+    if function_result == 1:
+        assert error_code_in_result("JG06", result)
 
 
 def test_is_not_function_too_long():
